cfd993500289cb7fa4a6128cfd9dccb881f54634
Angel D. Franco <angeldavidhf@gmail.com>
2023-03-30 18:01:53 -0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import About from "@pages/About";
 import Contact from "@pages/Contact";
+import LoginRegister from "@pages/LoginRegister";
 function App() {
   return (
     <>
         <Routes>
-            <Route path='/' element={<Home></Home>}>  </Route>
-            <Route path='about' element={<About></About>}>  </Route>
-            <Route path='contact' element={<Contact></Contact>}>  </Route>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='about' element={<About></About>}></Route>
+            <Route path='contact' element={<Contact></Contact>}></Route>
+            <Route path='login' element={<LoginRegister></LoginRegister>}></Route>
         </Routes>
     </> 
   )
